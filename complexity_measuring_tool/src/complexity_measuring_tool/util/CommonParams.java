23f6b22f7bf82bed70a00699684115ffb6acc80f
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-08-17 07:50:40 +0530

    Create Utility class to add Common prameters to entire project

---
+++ complexity_measuring_tool/src/complexity_measuring_tool/util/CommonParams.java
@@ -0,0 +1,11 @@
+package complexity_measuring_tool.util;
+
+
+
+public class CommonParams {
+	
+	public static final int ARRAY_FIRST_ELEMENT = 0;
+	public static final int ARRAY_SECOND_ELEMENT = 1;
+	public static final String LOCAL_UPLOAD_FILE_FOLDER_PATH = "C:/Users/Asus/Desktop/SPM Project/WE_47/complexity_measuring_tool/WebContent/uploaded-files/"; 
+
+}
