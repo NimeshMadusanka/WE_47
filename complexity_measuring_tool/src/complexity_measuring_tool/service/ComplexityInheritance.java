e5a0bccf5fc152b915117c558da08e1a96539d28
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-08-17 07:56:36 +0530

    Create class to calculate complexity inheritance of a programm and static value to access while doing other calculation, reset method also implemented to reset the static ci value

---
+++ complexity_measuring_tool/src/complexity_measuring_tool/service/ComplexityInheritance.java
@@ -0,0 +1,42 @@
+package complexity_measuring_tool.service;
+
+import complexity_measuring_tool.model.CheckFile;
+import complexity_measuring_tool.util.CommonParams;
+
+public class ComplexityInheritance {
+	
+	private static int complexityInheritanceValue=0;
+	
+	public static int calculateCi(CheckFile checkFile) {
+		System.out.println(checkFile.getDate());
+
+		for (String line : checkFile.getFileLines()) {
+			line.trim();// removing spaces
+				if(line.contains("implements ")) {
+					complexityInheritanceValue+=1;
+					String[] splitted= line.split("implements ");
+					String keyWordAfterString=splitted[CommonParams.ARRAY_SECOND_ELEMENT];
+					String[] beforCurly = keyWordAfterString.split("\\{");
+					String[] implementedSuper = beforCurly[CommonParams.ARRAY_FIRST_ELEMENT].split(",");
+					for (int i = 0; i < implementedSuper.length; i++) {
+						System.out.println(implementedSuper[i].trim());
+						complexityInheritanceValue+=1;
+					}
+				}
+				if(line.contains("extends ")) {
+					complexityInheritanceValue +=1;
+				}
+				if(line.contains("class ") || line.contains("interface ") ) {
+					complexityInheritanceValue+=1;
+					break;
+				}
+				
+		}
+		return complexityInheritanceValue;
+	}
+	
+	public static void resetCIValue() {
+		complexityInheritanceValue = 0;
+	}
+
+}
