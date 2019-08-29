b149b0da4951d7ec3cbbff0c861636ab17806550
RuwanPeiris <ruwanpeiris1991@gmail.com>
2019-08-29 10:52:09 +0530

    Nesting Complexity related works are done

--- complexity_measuring_tool/src/complexity_measuring_tool/service/ComplexityNesting.java
+++ complexity_measuring_tool/src/complexity_measuring_tool/service/ComplexityNesting.java
@@ -1,6 +1,10 @@
 package complexity_measuring_tool.service;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -16,24 +20,38 @@ public class ComplexityNesting {
 
 	static ArrayList<String> stack = new ArrayList<String>();
 	static MyStack myStack = new MyStack();
+	private static Logger logger = Logger.getLogger(ComplexityInheritance.class.getName()); 
 
 	public static int calculateNestingComplexity(CheckFile checkFile) {
 		int complexityNestingValue = 0;
-		System.out.println(checkFile.getDate());
+		
+		//File handler open to write logs 
+		FileHandler fileHandler=null;
+		try {
+			///
+			fileHandler = new FileHandler(CommonParams.LOG_FILE_PATH,true);
+			logger.addHandler(fileHandler);
+			SimpleFormatter formatter =new SimpleFormatter();
+			fileHandler.setFormatter(formatter);
+		} catch (SecurityException | IOException e) {
+			logger.info("Error occured while handling the file in "+CommonParams.LOG_FILE_PATH+" exception ocured due to "+e.getCause());
+			e.printStackTrace();
+		}
+		
+		//Pattern decelerations
 		final Pattern forLoopPattern = Pattern.compile(CommonParams.REGEX_FOR_LOOP);
 		final Pattern forEachPattern = Pattern.compile(CommonParams.REGEX_FOR_EACH);
 		final Pattern whileLoopPattern = Pattern.compile(CommonParams.REGEX_WHILE_LOOP);
 		final Pattern ifPattern = Pattern.compile(CommonParams.REGEX_IF);
 		final Pattern switchPattern = Pattern.compile(CommonParams.REGEX_SWITCH);
 		final Pattern elsePattern = Pattern.compile(CommonParams.REGEX_ELSE);
-
-		for (int i = 0; i < 10; i++) {
-
-		}
-
+		final Pattern lineEndsPattern = Pattern.compile(CommonParams.LINE_ENDS_WITH_CURLY);
+		final Pattern lineStartsPattern = Pattern.compile(CommonParams.LINE_STARTS_WITH_CURLY);
+		
+		
 		for (String line : checkFile.getFileLines()) {
 			line.trim();// removing all the leading and traling spaces in a line
-			System.out.println(line);
+			
 			if (null != line) {
 				Matcher forLoopMatcher = forLoopPattern.matcher(line);
 				if (forLoopMatcher.matches()) {
@@ -42,31 +60,42 @@ public class ComplexityNesting {
 				}
 				Matcher forEachMatcher = forEachPattern.matcher(line);
 				if (forEachMatcher.matches()) {
+					System.out.println("For each found syntax is " + line);
 					myStack.push(line);
 				}
 				Matcher whileLoopMatcher = whileLoopPattern.matcher(line);
 				if (whileLoopMatcher.matches()) {
+					System.out.println("while loop found syntax is " + line);
 					myStack.push(line);
 				}
 				Matcher ifMatcher = ifPattern.matcher(line);
 				if (ifMatcher.matches()) {
+					System.out.println("if found syntax is " + line);
 					myStack.push(line);
 				}
 				Matcher switchMatcher = switchPattern.matcher(line);
 				if (switchMatcher.matches()) {
+					System.out.println("Switch found syntax is " + line);
 					myStack.push(line);
 				}
 				Matcher elseMatcher = elsePattern.matcher(line);
 				if (elseMatcher.matches()) {
+					System.out.println("else found syntax is " + line);
 					myStack.push(line);
 				}
-				// removing closing brackets in the stack
-				if (myStack.peek() > 0 && line.startsWith("}") /* || line.endsWith("{") */) {
+				// removing finishing brackets in the stack
+				Matcher lineEndMatcher = lineEndsPattern.matcher(line);
+				Matcher lineStartMatcher = lineStartsPattern.matcher(line);
+				if (myStack.peek() > 0 && (lineEndMatcher.matches() || lineStartMatcher.matches() )){
 					myStack.pop();
 				}
+				System.out.println("Stack value : " + myStack.peek()+ line);
 				complexityNestingValue += myStack.peek();
 			}
 		}
+		logger.info("Total nesting complexity of "+checkFile.getFileName()+" is :"+ complexityNestingValue);
+		logger.info("-----------------------------------------------------------------------------");
+		fileHandler.close();
 		return complexityNestingValue;
 
 	}
--- complexity_measuring_tool/src/complexity_measuring_tool/util/CommonParams.java
+++ complexity_measuring_tool/src/complexity_measuring_tool/util/CommonParams.java
@@ -12,10 +12,12 @@ public class CommonParams {
 	public static final String LOCAL_UPLOAD_FILE_FOLDER_PATH = "C:/Users/Asus/Desktop/SPM/WE_47/complexity_measuring_tool/WebContent/uploaded-files/";
 	public static final String REGEX_FOR_LOOP = "(\\s+|)for(\\s+|)\\((\\s|)\\w+\\s+\\w+(\\s+|)=(\\s+|)\\d+;(\\s+|)\\w+(\\s+|)(\\<|\\>|\\<=|\\>=)(\\s+|)\\d+;(\\s+|)\\w+(\\++|\\--)\\)(\\s+|)\\{(\\s|\\n|)";
 	public static final String REGEX_FOR_EACH = "(\\s+|)for(\\s+|)\\((\\s|)\\w+\\s+\\w+(\\s+|):(\\s+|)\\w+(\\s+|)\\)(\\s+|)\\{(\\s|\\n|)";
-	public static final String REGEX_WHILE_LOOP = "(\\s+|)while(\\s+|)\\((\\s|)+((\\w+(\\s|)+(\\<|\\>|\\=|\\<=|\\>=)(\\s|)+(\\w+|\\d+|\\w+\\.\\w+\\((\\w+|\\d+)\\)))|(\\w+))(\\s+|)\\)(\\s+|)\\{(\\s|\\n|)";
+	public static final String REGEX_WHILE_LOOP = "(\\s+|)while(\\s+|)\\((\\s|)+((\\w+(\\s|)+(\\<|\\>|\\=|\\<=|\\>=)(\\s|)+(\\w+|\\d+|\\w+\\.\\w+\\((\\w+|\\d+)\\)))|(\\w+)|(\\w+\\.\\w+\\((\\w+|)\\)))(\\s+|)\\)(\\s+|)\\{(\\s|\\n|)";
 	public static final String REGEX_IF = "(\\s+|)if(\\s+|)\\((\\s|)+((\\w+(\\s|)+(\\<|\\>|\\=|\\<=|\\>=)(\\s|)+(\\w+|\\d+|\\w+\\.\\w+\\((\\w+|\\d+)\\)))|(\\w+))(\\s+|)\\)(\\s+|)\\{(\\s|\\n|)";
 	public static final String REGEX_SWITCH = "(\\s+|)switch(\\s+|)\\((\\s|)+((\"\\w+\")|(\'\\w\')|\\d|\\w)(\\s|)+\\)(\\s+|)\\{(\\s|\\n|)";
 	public static final String REGEX_ELSE = "(\\s+|)}(\\s+|)else(\\s+|)\\{(\\s|\\n|)";
 	public static final String REGEX_METHOD_SIGNATURE=  "^([_0-9a-zA-Z()]+)\\{";
+	public static final String LINE_ENDS_WITH_CURLY=  "(\\s+|)\\{(\\s+|)";
+	public static final String LINE_STARTS_WITH_CURLY=  "(\\s+|)\\}(\\s+|)";
 	public static final String LOG_FILE_PATH = "C:/COMPLEXITY/LOG/complexity.log";
 }
